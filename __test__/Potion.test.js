5735c618e25fd1136aaae79446fbcdeeeb1d93f0
CTripleF <cjfabra1@gmail.com>
2021-12-31 12:02:48 -0500

    update to use class instead of function

---
+++ __test__/Potion.test.js
@@ -0,0 +1,18 @@
+const Potion = require('../lib/Potion.js');
+
+// test for creation of new potion obtect with name and calue properties
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
+
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
+// tests creation of random potion
+test('creates a random potion object', () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
